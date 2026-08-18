297cb05e812bfc639443d42a84bca1f25ffd49fe
Tipeco Group <tipecogroup@gmail.com>
2026-08-18 15:54:39 +0200

    Update auth.js

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,1 +1,7 @@
+/* =====================================================
+   TIPECO GROUP - AUTHENTICATION JAVASCRIPT
+   Login & Register
+   Version: 1.0
+===================================================== */
 
+console.log("TIPECO GROUP Auth JS loaded successfully.");
